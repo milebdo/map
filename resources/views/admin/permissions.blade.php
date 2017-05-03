554dcfea6f5597cdb30814b6d5920bf545a1aa41
Riza <riza.fauzi.rahman@gmail.com>
2017-05-03 21:27:42 +0700

    Fix table

--- resources/views/admin/permissions.blade.php
+++ resources/views/admin/permissions.blade.php
@@ -17,7 +17,7 @@
             <a class="btn btn-success" href="{{ url('admin/permissions/form') }}"><i class="fa fa-ban"></i> {{ trans('backoffice.create_permission') }}</a>
         </p>
 
-        <table id="example" class="display table-striped" cellspacing="0" width="100%">
+        <table id="example" class="display table table-striped table-hover table-bordered" cellspacing="0" width="100%">
             <thead>
                 <tr>
                     <th>{{ trans('backoffice.label') }}</th>
